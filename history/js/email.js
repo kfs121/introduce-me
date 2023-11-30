49b6165c9538a64c2199cca50f06294668dffd4b
kfs121 <kfs121@naver.com>
2023-11-30 18:10:03 +0900

    email feature update

--- history/js/email.js
+++ history/js/email.js
@@ -1,17 +1,22 @@
 const $mailWrap = document.querySelector('#email-section .form-wrap');
 const $mailForm = $mailWrap.querySelector('form');
 const $sendButton = $mailFormWrap.querySelector('button');
+const $sendLoading = document.querySelector('#email-section .send__loading');
+const $sendSuccess = document.querySelector('#email-section .send__success');
 emailjs.init('C01RPPzdk0LpHSE8o');
 
 $mailForm.addEventListener('submit', (e)=>{
   e.preventDefault();
   
   if(!validate(e.target)) return;
-
+  $sendLoading.style.display = 'block';
   emailjs.sendForm('service_ve5g14c', 'template_9e20roe', e.target)
   .then(()=>{
     console.log('success');
+    $sendLoading.style.display = 'none';
+    $sendSuccess.style.display = 'block';
   }, (err)=>{
+    $sendLoading.style.display = 'none';
     console.log(err);
   })
   $mailWrap.style.animation = `ani-paper 1.5s linear forwards`
